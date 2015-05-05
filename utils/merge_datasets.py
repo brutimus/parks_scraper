332c805776a7584c7b603183bb9379360733b0a0
Sean Stoops <brutimus@gmail.com>
2015-05-05 15:51:12 -0700

    Added forecast support

--- utils/merge_datasets.py
+++ utils/merge_datasets.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
+import forecastio
 import json
 import petl
+
+from private import FORECAST_IO_API_KEY
+
+KNOTTS_LAT = 33.844317
+KNOTTS_LNG = -118.000227
+
+
+def get_forecast():
+    forecast = forecastio.load_forecast(
+        FORECAST_IO_API_KEY, KNOTTS_LAT, KNOTTS_LNG)
+    return {x.time.strftime('%Y-%m-%d'):x for x in forecast.daily().data}
 
 
 def main():
@@ -9,6 +21,7 @@ def main():
     knotts_spreadsheet = petl.fromcsv(
         'https://spreadsheets.google.com/tq?key=1_wmXgh1zuljwzH2O1eW_BTBkxDkxyckLzSD9cT4Kn2Q&gid=0&tqx=out:csv'
         )
+    forecast = get_forecast()
 
     spreadsheet_lookup = {}
     for item in knotts_spreadsheet.dicts():
@@ -20,16 +33,27 @@ def main():
     show_times_lookup = {}
     for item in show_times.dicts():
         show_times_lookup[item['date']] = show_times_lookup.get(
-            item['date'], []) + [{'times': item['times'], 'location': item['location']}]
+            item['date'], []) + [{
+                'name': item['name'],
+                'times': item['times'], 
+                'location': item['location']}]
 
     new_park_hours = []
     for day in park_hours.dicts():
+
         st = show_times_lookup.get(day['date'])
         if st:
             day['show_times'] = st
+
         sheet_item = spreadsheet_lookup.get(day['date'])
         if sheet_item:
             day.update(sheet_item)
+
+        forecast_item = forecast.get(day['date'])
+        if forecast_item:
+            day['temperatureMax'] = forecast_item.temperatureMax
+            day['temperatureMin'] = forecast_item.temperatureMin
+            day['weatherIcon'] = forecast_item.icon
         new_park_hours.append(day)
     with open('../data/merged_data.json', 'w+') as f:
         json.dump(new_park_hours, f)
